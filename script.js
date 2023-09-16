c44a7480eb064aaa2f83a764e71ded74c939a9ba
cinderrblock <103962472+cinderrblock@users.noreply.github.com>
2023-09-16 14:05:45 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+
+const imageFiles = [
+    'image1.jpg',
+    'image2.jpg',
+    'image3.jpg',
+
+];
+function getRandomImage() {
+    const randomIndex = Math.floor(Math.random() * imageFiles.length);
+    return imageFiles[randomIndex];
+}
+
+
+function displayRandomImage() {
+    const randomImage = getRandomImage();
+    const imageContainer = document.getElementById('image-container');
+    const randomImageElement = document.getElementById('random-image');
+    
+ 
+    randomImageElement.src = randomImage;
+
+
+
+
+    randomImageElement.addEventListener('click', function() {
+
+        alert(`You clicked on ${randomImage}`);
+    });
+}
+
+const loadImageButton = document.getElementById('load-image-button');
+loadImageButton.addEventListener('click', displayRandomImage);
